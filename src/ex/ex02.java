36c2f1c999da8de3e08c80da8dce8886833e5052
Shengjie0266 <105021040@live.asia.edu.tw>
2016-10-24 16:26:24 +0800

    ex
    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,23 @@
 /*
  * Topic: 讓使用者輸入一正整數n，用while迴圈計算n!後輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021040 洪聖傑
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		System.out.print("請輸入一個正整數:");
+		int n = scn.nextInt();
+		int sum=1,i=1;
+		while(i<=n){
+			sum=sum*i;
+			i++;
+					
+		}
+		System.out.println(sum);
 	}
 
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,21 @@
 /*
  * Topic: 讓使用者輸入一正整數 n，並輸出 2^1+2^2+2^3+...+2^n 的值。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021040 洪聖傑
  */
-
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入一個正整數:");
+		int n = scn.nextInt();
+		double sum =0;
+		for(int i=1;i<=n;i++){
+			sum +=Math.pow(2,i);
+		}
+		System.out.println(sum);
 	}
 
 }
